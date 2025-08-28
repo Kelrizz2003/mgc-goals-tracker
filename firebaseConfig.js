83d43b29af6bb007e7dee90a1c080f430f089b21
Jemvendie-Doven-Reyes <jemvendiedoven@gmail.com>
2025-08-28 11:47:32 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7ntpNom6QvDy0cPkrRsUt7Yd4xKF3RS8",
+  authDomain: "mgc-goal-tracker.firebaseapp.com",
+  projectId: "mgc-goal-tracker",
+  storageBucket: "mgc-goal-tracker.firebasestorage.app",
+  messagingSenderId: "445234825437",
+  appId: "1:445234825437:web:93c54858768205354f3334"
+};
+
+
+const app = initializeApp(firebaseConfig);
